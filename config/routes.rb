36f3384bb65b71850a63c17b1ce73ef1fe9ce4c4
Yaseer Anaisi Okino <37089367+yaseerokino@users.noreply.github.com>
2022-06-17 19:01:43 +0100

    Fix linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,21 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :v1, defaults: { format: 'json' } do
+    get 'greetings', to: 'greetings#index'
+  end
+
+  # Forward all requests to the StaticController#index but requests
+  # must be non-Ajax (!req.xhr?) and HTML Mime type (req.format.html?).
+  # This does not include the root ("/") path.
+
+  get '*path', to: 'static#index', constraints: lambda { |req|
+                                                  req.xhr? || req.format.html?
+                                                }
+
+  root 'static#index'
 end
